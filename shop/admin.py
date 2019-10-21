f34e079c13ab0a9804bc06c008cbabc895e6ea48
Atul Singh <atul.nitr@gmail.com>
2019-10-21 12:14:44 +0530

    showing products in category in admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,11 +1,28 @@
 from django.contrib import admin
 from .models import Product, Category
 
+
+class CatItemAdmin(admin.TabularInline):
+    model = Product
+    fieldsets = [
+        ('Product', {'fields': ['name'], }),
+        ('Price', {'fields': ['price'], }),
+    ]
+    readonly_fields = ['name', 'price']
+    can_delete = False # to remove delete option
+    max_num = 0 # to remove add option
+    template = 'admin/shop/tabular.html'
+
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'slug']
     prepopulated_fields = {'slug':('name',)}
     ordering = ['id']
     list_display_links = ['id', 'name']
+
+    inlines = [
+        CatItemAdmin,
+    ]
 admin.site.register(Category, CategoryAdmin)
 
 
